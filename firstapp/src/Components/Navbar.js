94cad32740b6f32a5db74ec66d29e13634c41ab3
ahmedzafarm14 <ahmedzafarm14@gmail.com>
2024-11-18 14:53:53 +0500

    first commit

---
+++ firstapp/src/Components/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Text Form
+        </a>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Products
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          <span className="navbar-text">
+            Navbar text with an inline element
+          </span>
+        </div>
+      </nav>
+    </div>
+  );
+};
